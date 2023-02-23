17823148c0ac819f168ce41abc15e8f223353e82
Александр Дунцев <a.duncev@open-s.info>
2023-02-23 10:08:50 +0300

    Add project files.

---
+++ ConsoleApp8/ComlimentHelper.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp8.Contracts;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+internal static class ComlimentHelper
+{
+	public static async Task<string> GetComliment()
+	{
+		using (var client = new HttpClient())
+		{
+			var response = await client.GetAsync("https://complimentr.com/api");
+			if (response.IsSuccessStatusCode)
+			{
+				var compliment = await response.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<Comliment>(compliment).Compliment;
+			}
+			else
+			{
+				throw new ApplicationException( $"Error retrieving compliment: {response.StatusCode}");
+			}
+		}
+	}
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+internal class Program
+{
+
+	internal static void Main(string[] args)
+	{
+		string token = "";
+		long kateChatId = 0;
+
+		ComplimentBot bot = new ComplimentBot(token, kateChatId);
+		bot.StartComplimentTimer();
+		Console.ReadKey();
+	}
+}
